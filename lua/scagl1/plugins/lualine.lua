4d76a4340f9aa96c8a617802b125835aeba10636
scagl1 <gugascaglione2@gmail.com>
2025-07-19 14:46:35 -0300

    update OS icon in lualine plugin

--- lua/scagl1/plugins/lualine.lua
+++ lua/scagl1/plugins/lualine.lua
@@ -63,7 +63,7 @@ return {
 						color = { fg = "#ff9e64" },
 					},
 					{ "encoding" },
-					{ "fileformat", symbols = { unix = "" } },
+					{ "fileformat", symbols = { unix = "󰌽", dos = "󰍲", mac = "" } },
 					{ "filetype" },
 				},
 			},
